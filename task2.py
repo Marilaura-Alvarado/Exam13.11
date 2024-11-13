cf00eef4772907ea15399013b82753d6ceaaf2b2
Marilaura-Alvarado <marilaura2001@gmail.com>
2024-11-13 19:32:50 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,5 +1,18 @@
-"""This is my file.
-But put here your code please
-instead of this text
-and send me a pull request
-so I will be able to review your code"""
+def is_power(a, b):
+    """
+    Checks if a is a power of b.
+    """
+    if a == 1:
+        return True
+    
+    if a < b or a % b !=0:
+        return False
+    
+    
+    return is_power(a // b, b)
+
+# Test
+print(is_power(16, 2))
+print(is_power(12,2))
+print(is_power(100000,10))
+print(is_power(990,9))
